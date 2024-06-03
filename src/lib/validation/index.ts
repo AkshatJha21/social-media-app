ebadfcc660966f088d063b559c5118740d3bc4a7
AkshatJha21 <akshatj2134@gmail.com>
2024-06-03 17:59:08 +0530

    cleanup

--- src/lib/validation/index.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-export const signupValidation = z.object({
-    name: z.string().min(2, { message: "Too short" }),
-    username: z.string().min(2, { message: "Too short" }),
-    email: z.string().email(),
-    password: z.string().min(8, { message: "Must be atleast 8 characters" })
-  })
